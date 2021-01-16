b4d4878560db52039a4575c5a0a0c2b497187211
ThalesLJ <thaleslimadejesus@gmail.com>
2021-01-16 14:28:52 -0300

    Configuração local de rotas e criação das páginas Login e Home

--- OrganizaTudo/src/app/app-routing.module.ts
+++ OrganizaTudo/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '', component: AppComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'login', component: LoginComponent }
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
