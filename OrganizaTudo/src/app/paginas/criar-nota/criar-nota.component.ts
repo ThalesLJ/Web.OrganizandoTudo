5fc6c857e8f34f14565806da42e6863f3b171152
ThalesLJ <thaleslimadejesus@gmail.com>
2021-01-25 12:53:13 -0300

    Implementação da funcionalidade de Criação de Notas

--- OrganizaTudo/src/app/paginas/criar-nota/criar-nota.component.ts
+++ OrganizaTudo/src/app/paginas/criar-nota/criar-nota.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/servicos/api.service';
 import { SessaoService } from 'src/app/servicos/sessao.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-nota',
@@ -9,10 +10,20 @@ import { SessaoService } from 'src/app/servicos/sessao.service';
 })
 export class CriarNotaComponent implements OnInit {
 
-  constructor(private api: ApiService, private sessao: SessaoService) { }
+  constructor(
+    private api: ApiService, private sessao: SessaoService,
+    private router: Router) { }
+
+  titulo = '';
+  nota = '';
 
   ngOnInit(): void {
     this.sessao.validarToken();
+  }
+
+  Salvar(): void {
+    const nota = { titulo: this.titulo, nota: this.nota };
+    this.api.CriarNota(nota).then((retorno) => { this.router.navigate(['/home/notas']); }).catch((retorno) => { });
   }
 
 }
